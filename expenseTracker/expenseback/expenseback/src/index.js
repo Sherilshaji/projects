202f73f8f720fc6ec46c8fc3cca09b9f1dfce3d3
Sherilshaji <137503985+Sherilshaji@users.noreply.github.com>
2024-12-21 13:54:11 +0530

    Added expenseback as a normal directory

---
+++ expenseTracker/expenseback/expenseback/src/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const routes = require('./routes');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Define your frontend URL
+const allowedOrigins = [
+  'https://67441d6f2fff0000087b6ea0--dashing-semifreddo-b93e1d.netlify.app',  // Netlify URL
+  'https://your-frontend-domain.com', // Add any other frontend URLs you want to allow
+  'http://localhost:3000', // Update this to match your frontend's port
+  'http://localhost:4200' // If you are also using Angular or another frontend on this port
+];
+
+
+// Configure CORS with specific origins
+const corsOptions = {
+  origin: true, // Allow all origins (not recommended for production)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+
+
+app.use(cors(corsOptions));  // Use the customized CORS configuration
+
+// Middleware
+app.use(bodyParser.json());
+
+// MongoDB Connection
+console.log('MongoDB URI:', process.env.MONGODB_URI);
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Atlas connected'))
+  .catch((err) => {
+    console.error('MongoDB connection error:', err.message);
+    process.exit(1); // Exit if DB connection fails
+  });
+
+// API Routes
+app.use('/api', routes);
+
+// Server Start
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
