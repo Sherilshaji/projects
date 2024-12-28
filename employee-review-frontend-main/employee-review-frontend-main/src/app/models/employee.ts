99389bbfc65d1ddad0d370666f95d33c31138aaf
Sherilshaji <137503985+Sherilshaji@users.noreply.github.com>
2024-12-28 19:41:16 +0530

    Deleted unwanted directories

--- employee-review-frontend-main/employee-review-frontend-main/src/app/models/employee.ts
+++
@@ -1,9 +0,0 @@
-export interface Employee {
-    _id: string;
-    full_name: string;
-    email: string;
-    password: string;
-    gender: string;
-    role: "admin" | "employee";
-    position?: string;
-}
--- employee-review-frontend-main/employee-review-frontend-main/src/app/models/feedback.ts
+++
@@ -1,4 +0,0 @@
-export interface Feedback {
-    rating: number;
-    feedback: string;
-}
